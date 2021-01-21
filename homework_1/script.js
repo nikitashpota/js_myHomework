eda27fe4c41e10ec393ab3ecf1adf803493ed29e
Nikita <nikitashpota@ya.ru>
2021-01-21 15:45:48 +0300

    add hw#1

---
+++ homework_1/script.js
@@ -0,0 +1,22 @@
+//Задание №1
+var degreeC = prompt('Введите значение температуры в C');
+alert(((9 / 5) * degreeC + 32) + " F");
+
+//Задание №2
+var name = 'Василий';
+var admin = name;
+alert(admin);
+
+//Доп задание №1
+alert(1000 + '108')
+
+//Доп задание №3
+var a = +prompt('Введите A');
+var b = +prompt('Введите B');
+
+a = [a, b];
+b = a[0];
+a = a[1];
+
+alert("A = " + a);
+alert("B = " + b); 
